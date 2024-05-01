8ba98552a9dbdd8d7ec368b67f202ab535f386f9
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-05-01 14:15:55 +0100

    Modify the entry point of app container

--- scripts/python environment.sh
+++ scripts/python environment.sh
@@ -3,4 +3,4 @@ python3 -m pip install --upgrade pip
 pip install -r /app/requirements.txt
 sleep 15
 python /app/utils/create_kafka_topics.py
-python /app/start_stream.py comments
+python /app/main.py comments
