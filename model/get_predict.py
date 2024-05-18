217af44e4e32c7a098884df0024e6a1e9121b726
papew28 <cissepapewaly@gmail.com>
2024-05-18 14:36:45 +0100

    update

--- model/get_predict.py
+++ model/get_predict.py
@@ -1,11 +1,18 @@
-def get_predictions(title,post,commentaire, model, tokenizer):
-    id2label={
-          0:"neutral",
-          1:"with palestine",
-          2:"with israel"
-    }
+def get_predictions(title,post,commentaire,saved_model):
+    from transformers import DistilBertTokenizer,AutoModelForSequenceClassification
+    import torch
+    saved_model=torch.load(f"{saved_model}")
+    tokenizer=DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+    model =  AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased',
+                                                      problem_type="multi_label_classification", 
+                                                      num_labels=3,                  
+                                                     )
+    model.load_state_dict(state_dict)
+    device = torch.device("cpu")
+    model.to(device)
+    
     model.eval()
-    inputs = tokenizer("title: "+title+"\n"+"post: "+post+"\n"+"comment: "+commentaire, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    inputs = tokenizer("title of the post: "+title+"\n"+"post: "+post+"\n"+"comment: "+commentaire, return_tensors="pt", padding=True, truncation=True, max_length=512)
     input_ids = inputs['input_ids'].to(device)
     attention_mask = inputs['attention_mask'].to(device)
 
@@ -13,5 +20,10 @@ def get_predictions(title,post,commentaire, model, tokenizer):
         outputs = model(input_ids, attention_mask=attention_mask)
         logits = outputs.logits
         _, preds = torch.max(logits, dim=1)
-    
+    id2label={
+          0:"neutral",
+          1:"with palestine",
+          2:"with israel"
+    }
+    print(id2label)
     return id2label[preds.item()]
